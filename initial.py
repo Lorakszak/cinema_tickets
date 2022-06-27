cdc004c61a2e2fc366f156e045df3ee04e95b94e
Lorakszak <Lorakszak@gmail.com>
2022-06-27 07:05:37 +0200

    init commit part2

---
+++ initial.py
@@ -0,0 +1,116 @@
+# Code for initialization/clean-up
+
+# import section
+from cassandra.cluster import Cluster
+from time import sleep
+
+# DB info
+node_1 = '172.17.0.2'
+node_2 = '172.17.0.3'
+node_3 = '172.17.0.4' # TODO: Not used -> over memory capabilities
+
+port_1 = 9042
+port_2 = 9043
+port_3 = 9044 # TODO: Not used -> over memory capabilities
+
+# Specific theater features
+rooms = ['A', 'B', 'C', 'D', 'E', 'F']
+plays = ['Pirates', 'Romeo and Juliet', 'Boring sp z o o', 'Waiting for Godot', 'Oda', 'Shrek The greener the better']
+rows = 12
+places = 10
+
+
+# Connecting to the cluster + checking if exists/creating keyspace for the project
+cluster = Cluster([node_1]) # node_2 -> TODO
+session = cluster.connect()
+
+keyspaces = session.execute(
+    """
+    SELECT * FROM system_schema.keyspaces;
+    """
+)
+keyspaces = [ans[0] for ans in keyspaces]
+
+if 'theater' not in keyspaces: # create keyspace
+    print('Keyspace dont yet exists, creating keyspace "theater"')
+    session.execute("""
+                CREATE KEYSPACE theater
+                WITH REPLICATION = { 
+                'class' : 'NetworkTopologyStrategy',
+                'datacenter1' : 2
+                };
+                """) # TODO: replication to 3 if 3 nodes used
+else: # keyspace already exists -> delete it and reinitialize
+    session.execute('DROP KEYSPACE theater')
+    print('Keyspace successfully deleted')
+    sleep(3)
+    session.execute("""
+                CREATE KEYSPACE theater
+                WITH REPLICATION = { 
+                'class' : 'NetworkTopologyStrategy',
+                'datacenter1' : 2
+                };
+                """) # TODO: replication to 3 if 3 nodes used
+    print('Keyspace theater successfully reinitialized')
+
+# session.set_keyspace("theater") # equivalent to session.execute("USE theater")
+
+# Creating first table for the project if not already exist (plays)
+session.execute(
+    """
+    CREATE TABLE IF NOT EXISTS theater.plays (
+    room text,
+    play text,
+    PRIMARY KEY ((room), play)
+    )
+    """
+)
+print('Table "plays" successfully created')
+
+
+# Creating second table for the project if not already exist (rooms)
+session.execute(
+    """
+    CREATE TABLE IF NOT EXISTS theater.rooms (
+    room text,
+    row smallint,
+    place smallint,
+    occupied text,
+    client text,
+    PRIMARY KEY ((room), row, place)
+);
+    """
+)
+print('Table "rooms" successfully created')
+
+# # As the project states we have a theater with 6 rooms, each room have 12 rows and 10 places in each row (720 places)
+# # Now we intend to fill in the table with all available rooms and places and initialize them all to empty (not occupied)
+
+# # Filling table with initial data (plays)
+for idx, room in enumerate(rooms):
+    session.execute(
+        """
+        INSERT INTO theater.plays (room, play)
+        VALUES(%s, %s)
+        """, 
+        (room, plays[idx])
+    )
+
+print('Table "rooms" successfully filled')
+
+
+# # Filling table with initial data (rooms)
+for room in rooms:
+    for row in range(rows):
+        for col in range(places):
+            session.execute(
+                """
+                INSERT INTO theater.rooms (room, row, place, occupied, client)
+                VALUES(%s, %s, %s, %s, %s)
+                """, 
+                (room, row+1, col+1, 'no', '-')
+            )
+
+print('Table "rooms" successfully filled')
+
+
---
+++ main.py
@@ -0,0 +1,137 @@
+import numpy as np
+import pandas as pd
+
+from cassandra.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
+from cassandra.policies import WhiteListRoundRobinPolicy, DowngradingConsistencyRetryPolicy
+from cassandra.query import tuple_factory
+
+# profile = ExecutionProfile(
+#     load_balancing_policy=WhiteListRoundRobinPolicy(['127.0.0.1']),
+#     retry_policy=DowngradingConsistencyRetryPolicy(),
+#     consistency_level=ConsistencyLevel.LOCAL_QUORUM,
+#     serial_consistency_level=ConsistencyLevel.LOCAL_SERIAL,
+#     request_timeout=15,
+#     row_factory=tuple_factory
+# )
+# cluster = Cluster(execution_profiles={EXEC_PROFILE_DEFAULT: profile})
+# session = cluster.connect()
+
+# print(session.execute("SELECT release_version FROM system.local").one())
+
+# profile_long = ExecutionProfile(request_timeout=30)
+# cluster = Cluster(execution_profiles={'long': profile_long})
+# session = cluster.connect()
+# session.execute(statement, execution_profile='long')
+
+# DB info
+node_1 = '172.17.0.2'
+node_2 = '172.17.0.3'
+node_3 = '172.17.0.4'
+
+port_1 = 9042
+port_2 = 9043
+port_3 = 9044
+
+# Specific theater features
+rows = 12
+places = 10
+
+# Connecting to the cluster + checking if exists/creating keyspace for the project
+cluster = Cluster([node_1]) # node_2 -> TODO
+session = cluster.connect()
+
+# Actual main loop
+client_name = input('Your name? > ')
+while True:
+    decision = input("Do you want to book some play? (y/n)> ")
+
+    if decision == 'n':
+        break
+
+    # 1) Check available plays, choose one
+    plays = [(play, room) for room, play in session.execute(
+        """
+        SELECT * FROM theater.plays;
+        """
+    )]
+
+    choice_play = int(input(
+        f"""
+        What play would you like to watch?
+        "1" -> {plays[0][0]}
+        "2" -> {plays[1][0]}
+        "3" -> {plays[2][0]}
+        "4" -> {plays[3][0]}
+        "5" -> {plays[4][0]}
+        "6" -> {plays[5][0]}
+        """
+    ))
+
+    chosen_play = plays[choice_play-1][0]
+    chosen_room = plays[choice_play-1][1]
+
+    # print(chosen_play, chosen_room)
+    # print(plays)
+
+    # 2) Check available places for chosen play, choose one/more places
+
+    places = [(row.room, row.row, row.place, row.occupied, row.client) for row in session.execute(
+        """
+        SELECT * FROM theater.rooms
+        WHERE room=%s;
+        """,
+        chosen_room
+    )]
+    print(len(places), places[0:5])
+
+    # Filling room occupancy for visualization
+    room_scheme = np.zeros([12, 10])
+    for place in places:
+        if place[3] == 'no': # not occupied place
+            room_scheme[place[1]-1, place[2]-1] = 1 # 1=available
+        else: # occupied place
+            room_scheme[place[1]-1, place[2]-1] = 0 # 0=unavailable
+    room_occupancy = pd.DataFrame(room_scheme)
+    print('Place availability on given play:')
+    print(room_occupancy)
+
+    chosen_places = input(
+        """
+        Choose places you want to book:
+        (in form "row,col row,col row,col...")
+        """
+    ).split(' ')
+
+    chosen_places = [el.split(',') for el in chosen_places]
+    chosen_places = [[int(el[0]), int(el[1])] for el in chosen_places]
+    print(chosen_places)
+
+    # 3) Booking chosen places:
+
+    # Make sure availibilty of given place -> and book available stated by the client:
+    for chosen_place in chosen_places:
+        place_occupancy = session.execute(
+        """
+        SELECT * FROM theater.rooms
+        WHERE room = %s AND row = %s AND place = %s;
+        """,
+        [chosen_room, chosen_place[0]+1, chosen_place[1]+1]
+        )
+
+        for row in place_occupancy:
+            place_occupancy = row.occupied
+            print(chosen_place, 'occupied? :', place_occupancy)
+
+        if place_occupancy =='yes': # place already occupied
+            print('We are sorry, but place', chosen_place, 'you marked to book is already taken, choose a different one!')
+        else: # place not yet booked
+            session.execute(
+                """
+                INSERT INTO theater.rooms (room, row, place, occupied, client)
+                VALUES(%s, %s, %s, %s, %s)
+                """,
+                (chosen_room, chosen_place[0]+1, chosen_place[1]+1, 'yes', client_name)
+            )
+
+
+
